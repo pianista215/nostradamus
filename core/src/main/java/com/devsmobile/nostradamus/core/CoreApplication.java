ae512b111f28359ac91a4a0cb66f592cbd097219
pianista215 <unaisarasola@hotmail.com>
2016-01-12 22:17:04 +0100

    Base with Angular 2 to create the web application

--- core/src/main/java/com/devsmobile/nostradamus/core/CoreApplication.java
+++ core/src/main/java/com/devsmobile/nostradamus/core/CoreApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CoreApplication {
 	
-	@RequestMapping("/")
+	@RequestMapping("/status")
     public String index() {
         return "Core working";
     }
